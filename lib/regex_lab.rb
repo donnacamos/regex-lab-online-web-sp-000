80658e1fea0d9da011c18ad0fc7b3f22959baa5c
Donna Amos <donnacamos.developer@gmail.com>
2019-03-07 00:59:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,8 +15,10 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z].*\.$/) 
-return 
+text.match(/^[A-Z].*\.$/) == nil
+return false
+else 
+  return true 
 end
 
 def valid_phone_number?(phone)
